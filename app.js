7b0b38e4df0de24b9dd9f319fc073f513fe914c6
Wyatt B <wyattbushman117@gmail.com>
2022-01-29 23:19:48 -0500

    bug fix on clr

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let calculate=(number)=>{
 
 //Works with all common calculations
 
-/*
+
 
 let Result=()=>{
     try{
@@ -19,10 +19,10 @@ let Result=()=>{
     }
 }
 
-*/
+
 
 function clr(){
-    result.value= '0';
+    result.value= '';
 }
 
 function del(){
@@ -31,53 +31,7 @@ function del(){
 }
 
 
-//Adding
-const add = function(number) {
-    result.value+=number;
-   };
-   
-//Subtracting
-const subtract = function(val1,val2) {
-    console.log(val1-val2)
-   };
-   
-   
-//Multiplying
-const multiply = function(val1,val2) {
-    console.log(val1 * val2)
-   };
-   
-//Dividing
-const divide = function(val1, val2){
-    console.log(val1/val2)
-};
 
-//Exponents
-const power = function(val1,val2) {
-    console.log(val1 ** val2)
-   };
-   
-   
-//Factorials
-const factorial = function(val) {
-   // Factorials can't be less than zero
-   if (val < 0) {
-       console.log('Error');
-   }
-   // Factorial of zero is 1
-   else if (val === 0) {
-       console.log(`The factorial of ${val} is 1.`);
-   }
-   // For every positive number n! = n × (n−1)!
-   // Test case 4 = 24 Eg: 4! = 4 × 3 × 2 × 1 = 24
-   else {
-       let fact = 1;
-       for (i = 1; i <= val; i++) {
-           fact *= i;
-       }
-       console.log(fact);
-   }
-   };
    
    
 
